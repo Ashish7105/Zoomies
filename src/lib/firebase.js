1f675f3722805efea8e120e8dae2242676530248
Ashish <ashishkum7105@gmail.com>
2026-02-18 18:55:05 +0530

    env setip

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHWaAy32tlxBoSf7gZewjkb99cjrdMXVI",
-  authDomain: "zoomies-1d011.firebaseapp.com",
-  projectId: "zoomies-1d011",
-  storageBucket: "zoomies-1d011.firebasestorage.app",
-  messagingSenderId: "904334744873",
-  appId: "1:904334744873:web:81093bddc8002660c72b38",
-  measurementId: "G-KXBGX4QDX9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
